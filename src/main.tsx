744603c415055448975aa096052723b40a381a16
Muhammad Yousuf <khanyousufmy80@gmail.com>
2023-12-05 01:01:39 +0500

    chores: app fonts

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,9 @@
+import '@fontsource/poppins'
+import '@fontsource/poppins/500.css'
+import '@fontsource/poppins/600.css'
+import '@fontsource/poppins/700.css'
+import '@fontsource/poppins/800.css'
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
@@ -6,5 +12,5 @@ import './index.css'
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 )
