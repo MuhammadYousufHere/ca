dbeb63b7e3cfc9a959ea50097d6621e150b5918a
Muhammad Yousuf <khanyousufmy80@gmail.com>
2023-12-04 17:09:29 +0500

    chores: configure import allias

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import './App.css'
+import { Button, Keyboard } from '@/components'
 
 function App() {
   return (
     <>
       <div>hello</div>
+      <Keyboard />
+      <Button />
     </>
   )
 }
---
+++ src/components/index.ts
@@ -0,0 +1,2 @@
+export { Keyboard } from './Keyboard'
+export { Button } from './Button'
