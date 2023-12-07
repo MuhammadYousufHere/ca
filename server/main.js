136d62ad5a0acef72913121b265116a9dcf6a6c2
Muhammad Yousuf <khanyousufmy80@gmail.com>
2023-12-07 23:42:34 +0500

    feat: complete curd for customer

--- server/main.js
+++ server/main.js
@@ -4,52 +4,28 @@ import mysql from 'mysql2'
 
 const app = express()
 
-app.use(express.json())
+app.use(express.json({ limit: '5mb' }))
 app.use(cors())
 
 const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: 'password123@',
-  database: 'customers',
+  database: 'saviynt',
 })
-
-db.connect((err) => {
-  if (err) {
-    console.error('Unable to connect to DB:', err)
-  } else {
-    console.log('Connected to MySQL database')
-    createTable()
-  }
-})
-// Create customers table if not exists
-function createTable() {
-  const createTableQuery = `
-    CREATE TABLE IF NOT EXISTS customers (
-      id INT AUTO_INCREMENT PRIMARY KEY,
-      name VARCHAR(255) NOT NULL,
-      email VARCHAR(255) NOT NULL,
-      profilePic VARCHAR(255)
-    )
-  `
-
-  db.query(createTableQuery, (err) => {
-    if (err) {
-      console.error('Error creating table:', err)
-    } else {
-      console.log('Table created or already exists')
-    }
-  })
-}
 
 app.get('/customers', (req, res) => {
-  const getAllCustomersQuery = 'SELECT * FROM customers'
+  const getAllCustomersQuery = 'SELECT * FROM Customers'
 
   db.query(getAllCustomersQuery, (err, results) => {
     if (err) {
       res.status(500).send('Error retrieving customers')
     } else {
-      res.json(results)
+      res.json({
+        data: results,
+        statusCode: 200,
+        message: 'successfully retrieved the customers.',
+      })
     }
   })
 })
@@ -57,7 +33,7 @@ app.get('/customers', (req, res) => {
 // Get customer by ID
 app.get('/customers/:id', (req, res) => {
   const customerId = req.params.id
-  const getCustomerQuery = 'SELECT * FROM customers WHERE id = ?'
+  const getCustomerQuery = 'SELECT * FROM Customers WHERE id = ? LIMIT 5'
 
   db.query(getCustomerQuery, [customerId], (err, results) => {
     if (err) {
@@ -65,7 +41,11 @@ app.get('/customers/:id', (req, res) => {
     } else if (results.length === 0) {
       res.status(404).send('Customer not found')
     } else {
-      res.json(results[0])
+      res.status(200).json({
+        data: results[0],
+        statusCode: 200,
+        message: 'successfully retrieved the customers.',
+      })
     }
   })
 })
@@ -73,14 +53,30 @@ app.get('/customers/:id', (req, res) => {
 // Create a new customer
 app.post('/customers', (req, res) => {
   const { name, email, profilePic } = req.body
-  const createCustomerQuery = `INSERT INTO customers (name, email, profilePic) VALUES (?, ?, ?)`
+  const createCustomerQuery =
+    'INSERT INTO Customers (name, email, profilePic) VALUES (?, ?, ?)'
 
   db.query(createCustomerQuery, [name, email, profilePic], (err, result) => {
     if (err) {
-      console.log(err)
-      res.status(500).send('Error creating customer')
+      console.error(err)
+
+      if (err.code === 'ER_DUP_ENTRY') {
+        res.status(400).json({
+          message: 'Email address already exists',
+          statusCode: 400,
+        })
+      } else {
+        res.status(500).json({
+          message: 'Error creating customer',
+          statusCode: 500,
+        })
+      }
     } else {
-      res.json({ id: result.insertId, name, email, profilePic })
+      res.status(201).json({
+        message: 'Successfully created the customer',
+        statusCode: 201,
+        data: { id: result.insertId, name },
+      })
     }
   })
 })
@@ -88,20 +84,37 @@ app.post('/customers', (req, res) => {
 // Update customer by ID
 app.put('/customers/:id', (req, res) => {
   const customerId = req.params.id
-  const { name, email, profilePic } = req.body
-  const updateCustomerQuery =
-    'UPDATE customers SET name = ?, email = ?, profilePic = ? WHERE id = ?'
+
+  const allowedFields = ['name', 'email', 'profilePic']
+
+  const setClauses = Object.keys(req.body)
+    .filter((field) => allowedFields.includes(field))
+    .map((field) => `${field} = ?`)
+    .join(', ')
+
+  if (!setClauses) {
+    return res.status(400).send('No valid fields provided for update')
+  }
+
+  const updateCustomerQuery = `UPDATE Customers SET ${setClauses} WHERE id = ?`
+  const updateValues = allowedFields
+    .map((field) => req.body[field])
+    .filter((v) => v)
 
   db.query(
     updateCustomerQuery,
-    [name, email, profilePic, customerId],
+    [...updateValues, customerId],
     (err, result) => {
       if (err) {
         res.status(500).send('Error updating customer')
       } else if (result.affectedRows === 0) {
-        res.status(404).send('Customer not found')
+        res.status(404).json({ message: 'Customer not found', statusCode: 404 })
       } else {
-        res.json({ id: customerId, name, email, profilePic })
+        res.status(200).json({
+          data: { id: customerId, ...req.body },
+          message: 'update the customer information',
+          statusCode: 200,
+        })
       }
     }
   )
@@ -110,7 +123,7 @@ app.put('/customers/:id', (req, res) => {
 // Delete customer by ID
 app.delete('/customers/:id', (req, res) => {
   const customerId = req.params.id
-  const deleteCustomerQuery = 'DELETE FROM customers WHERE id = ?'
+  const deleteCustomerQuery = 'DELETE FROM Customers WHERE id = ?'
 
   db.query(deleteCustomerQuery, [customerId], (err, result) => {
     if (err) {
@@ -118,11 +131,21 @@ app.delete('/customers/:id', (req, res) => {
     } else if (result.affectedRows === 0) {
       res.status(404).send('Customer not found')
     } else {
-      res.send('Customer deleted successfully')
+      res
+        .status(200)
+        .json({ message: 'Customer deleted successfully', statusCode: 200 })
     }
   })
 })
 
 app.listen(3000, () => {
   console.log('server is running!')
+  console.log('Connecting to the database...')
+  db.connect(async (err) => {
+    if (err) {
+      console.error('Error connecting to MySQL:', err)
+      return
+    }
+    console.log('Successfuly connected to the database!')
+  })
 })
