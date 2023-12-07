1eecc6130aeea56a44ba584512aeb55a83846378
Muhammad Yousuf <khanyousufmy80@gmail.com>
2023-12-08 02:52:10 +0500

    feat: skeleton table

---
+++ src/pages/Customers/atoms/TableSkeleton.tsx
@@ -0,0 +1,126 @@
+import './skeleton.css'
+
+export function TableSkeleton() {
+  return (
+    <div className='container'>
+      <table className='tg'>
+        <thead>
+          <tr>
+            <th className='tg-cly1'>
+              <div className='line'></div>
+            </th>
+            <th className='tg-cly1'>
+              <div className='line'></div>
+            </th>
+            <th className='tg-cly1'>
+              <div className='line'></div>
+            </th>
+            <th className='tg-cly1'>
+              <div className='line'></div>
+            </th>
+            <th className='tg-cly1'>
+              <div className='line'></div>
+            </th>
+            <th className='tg-cly1'>
+              <div className='line'></div>
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td style={{ background: 'transparent' }}></td>
+          </tr>
+          <tr>
+            <td className='tg-cly1'>
+              <div className='square'></div>
+            </td>
+            <td className='tg-cly1'>
+              <div className='line'></div>
+            </td>
+            <td className='tg-cly1'>
+              <div className='line'></div>
+            </td>
+            <td className='tg-cly1'>
+              <div className='line'></div>
+            </td>
+            <td className='tg-cly1'>
+              <div className='btn_sk'></div>
+            </td>
+            <td className='tg-cly1'>
+              <div className='btn_sk'></div>
+            </td>
+          </tr>
+          <tr>
+            <td style={{ background: 'transparent' }}></td>
+          </tr>
+          <tr>
+            <td className='tg-cly1'>
+              <div className='square'></div>
+            </td>
+            <td className='tg-cly1'>
+              <div className='line'></div>
+            </td>
+            <td className='tg-cly1'>
+              <div className='line'></div>
+            </td>
+            <td className='tg-cly1'>
+              <div className='line'></div>
+            </td>
+            <td className='tg-cly1'>
+              <div className='btn_sk'></div>
+            </td>
+            <td className='tg-cly1'>
+              <div className='btn_sk'></div>
+            </td>
+          </tr>
+          <tr>
+            <td style={{ background: 'transparent' }}></td>
+          </tr>
+          <tr>
+            <td className='tg-cly1'>
+              <div className='square'></div>
+            </td>
+            <td className='tg-cly1'>
+              <div className='line'></div>
+            </td>
+            <td className='tg-cly1'>
+              <div className='line'></div>
+            </td>
+            <td className='tg-cly1'>
+              <div className='line'></div>
+            </td>
+            <td className='tg-cly1'>
+              <div className='btn_sk'></div>
+            </td>
+            <td className='tg-cly1'>
+              <div className='btn_sk'></div>
+            </td>
+          </tr>
+          <tr>
+            <td style={{ background: 'transparent' }}></td>
+          </tr>
+          <tr>
+            <td className='tg-cly1'>
+              <div className='square'></div>
+            </td>
+            <td className='tg-cly1'>
+              <div className='line'></div>
+            </td>
+            <td className='tg-cly1'>
+              <div className='line'></div>
+            </td>
+            <td className='tg-cly1'>
+              <div className='line'></div>
+            </td>
+            <td className='tg-cly1'>
+              <div className='btn_sk'></div>
+            </td>
+            <td className='tg-cly1'>
+              <div className='btn_sk'></div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  )
+}
